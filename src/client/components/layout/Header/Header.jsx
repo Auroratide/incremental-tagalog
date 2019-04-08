4f71cb85d7e14eaa816bcde4293c815be2553216
auroratide <tf.auroratide@gmail.com>
2019-04-08 16:33:19 -0500

    Better header

--- src/client/components/layout/Header/Header.jsx
+++ src/client/components/layout/Header/Header.jsx
@@ -6,7 +6,7 @@ import Container from 'Client/components/layout/Container';
 const Header = () =>
   <header className={styles.header}>
     <Container.nav>
-      Tagalog Learner
+      <span className={styles.title}>Tagalog Learner</span>
     </Container.nav>
   </header>;
 
