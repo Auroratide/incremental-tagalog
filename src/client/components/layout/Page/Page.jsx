77fa114f2233b94565ee8c8fc6dfed4001bb75da
auroratide <tf.auroratide@gmail.com>
2019-04-08 11:28:38 -0500

    Use fragment instead of naked div

--- src/client/components/layout/Page/Page.jsx
+++ src/client/components/layout/Page/Page.jsx
@@ -6,11 +6,11 @@ import Main from 'Client/components/layout/Main';
 import Footer from 'Client/components/layout/Footer';
 
 const Page = ({ children }) =>
-  <div>
+  <React.Fragment>
     <Header />
     <Main>{children}</Main>
     <Footer />
-  </div>;
+  </React.Fragment>;
 
 Page.propTypes = {
   children: PropTypes.node
