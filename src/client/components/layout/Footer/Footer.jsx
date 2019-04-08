0708a019078ca3a8db4828458d8cabfcb0ca153d
auroratide <tf.auroratide@gmail.com>
2019-04-08 11:32:13 -0500

    License in footer

--- src/client/components/layout/Footer/Footer.jsx
+++ src/client/components/layout/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from './style';
 
 const Footer = () =>
   <footer className={styles.footer}>
-    by Timothy Foster
+    <p>&copy; {new Date().getFullYear()} Timothy Foster, All Rights Reserved</p>
   </footer>;
 
 export default Footer;
