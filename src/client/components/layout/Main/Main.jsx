4e1f37e8fb3c3f382a97d0a41385774ae4e477b9
auroratide <tf.auroratide@gmail.com>
2019-04-10 10:11:04 -0500

    Spacing for footer

--- src/client/components/layout/Main/Main.jsx
+++ src/client/components/layout/Main/Main.jsx
@@ -3,8 +3,10 @@ import PropTypes from 'Client/prop-types';
 
 import Container from 'Client/components/layout/Container';
 
+import styles from './style';
+
 const Main = ({ children }) =>
-  <main role="main">
+  <main role="main" className={styles.main}>
     <Container>
       {children}
     </Container>
